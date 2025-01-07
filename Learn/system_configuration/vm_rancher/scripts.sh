a75c7475f4578b1b26508a8a3e812c6429e53166
Le Nguyen Ngoc <lebkp04@gmail.com>
2025-01-08 02:18:32 +0700

    fix: install docker before install rancher sv

--- Learn/system_configuration/vm_rancher/scripts.sh
+++ Learn/system_configuration/vm_rancher/scripts.sh
@@ -1,2 +1,7 @@
+#Install Docker
+sudo apt-get update
+curl -fsSL https://get.docker.com | sh
+sudo usermod -aG docker lenn
+
 #Install Rancher to monitoring k8s cluster 
 docker run --name rancher-server -d --restart=unless-stopped -p 6860:80 -p 6868:443 --privileged rancher/rancher:v2.7.6
