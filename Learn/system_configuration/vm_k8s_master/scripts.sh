cf88f74d72ed75603bec10042dc997231dd5bbb9
nguyenle <lenn@falcongames.com>
2025-01-15 16:47:58 +0700

    fix: fix unhealthy controller manager

--- Learn/system_configuration/vm_k8s_master/scripts.sh
+++ Learn/system_configuration/vm_k8s_master/scripts.sh
@@ -4,3 +4,7 @@ sudo cp /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
 
+#inf controller manager unhealthy, run this on masternode
+sudo sed -i 's|- --port=0|#- --port=0|' /etc/kubernetes/manifests/kube-scheduler.yaml
+sudo sed -i 's|- --port=0|#- --port=0|' /etc/kubernetes/manifests/kube-controller-manager.yaml
+sudo systemctl restart kubelet
