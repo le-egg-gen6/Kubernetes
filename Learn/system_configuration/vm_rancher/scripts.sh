2d2897da4f27624abc85cfbeeba08888600cd9fb
nguyenle <lenn@falcongames.com>
2025-01-15 16:45:49 +0700

    fix: restore to stable k8s& rancher version

--- Learn/system_configuration/vm_rancher/scripts.sh
+++ Learn/system_configuration/vm_rancher/scripts.sh
@@ -4,4 +4,4 @@ curl -fsSL https://get.docker.com | sh
 sudo usermod -aG docker lenn
 
 #Install Rancher to monitoring k8s cluster 
-docker run --name rancher-server -d --restart=unless-stopped -p 6860:80 -p 6868:443 --privileged rancher/rancher:v2.7.6
+docker run --name rancher-server -d --restart=unless-stopped -p 6860:80 -p 6868:443 --privileged rancher/rancher:v2.5.7
